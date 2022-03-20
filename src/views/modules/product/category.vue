9bace2d2fc1ff39955a4897b1005901e5b0285f5
Administrator <youngwah@yeah.net>
2022-03-21 00:56:13 +0800

    product前后端联通

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,49 +1,15 @@
 <template>
-  <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+  <el-tree :data="menus" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
 </template>
 
 <script>
   export default {
     data () {
       return {
-        data: [{
-          label: '一级 1',
-          children: [{
-            label: '二级 1-1',
-            children: [{
-              label: '三级 1-1-1'
-            }]
-          }]
-        }, {
-          label: '一级 2',
-          children: [{
-            label: '二级 2-1',
-            children: [{
-              label: '三级 2-1-1'
-            }]
-          }, {
-            label: '二级 2-2',
-            children: [{
-              label: '三级 2-2-1'
-            }]
-          }]
-        }, {
-          label: '一级 3',
-          children: [{
-            label: '二级 3-1',
-            children: [{
-              label: '三级 3-1-1'
-            }]
-          }, {
-            label: '二级 3-2',
-            children: [{
-              label: '三级 3-2-1'
-            }]
-          }]
-        }],
+        menus: [],
         defaultProps: {
           children: 'children',
-          label: 'label'
+          label: 'name'
         }
       }
     },
@@ -56,7 +22,8 @@
           url: this.$http.adornUrl('/product/category/list/tree'),
           method: 'get'
         }).then(data => {
-          console.log('get menu date success: ', data)
+          console.log(data)
+          this.menus = data.data.data
         })
       }
     },
