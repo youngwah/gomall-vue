e9e54bdcded383e6989daf9c86af985e4afbb4a0
Administrator <youngwah@yeah.net>
2022-03-21 00:40:20 +0800

    前后端联通

---
+++ src/views/modules/product/category.vue
@@ -0,0 +1,71 @@
+<template>
+  <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        data: [{
+          label: '一级 1',
+          children: [{
+            label: '二级 1-1',
+            children: [{
+              label: '三级 1-1-1'
+            }]
+          }]
+        }, {
+          label: '一级 2',
+          children: [{
+            label: '二级 2-1',
+            children: [{
+              label: '三级 2-1-1'
+            }]
+          }, {
+            label: '二级 2-2',
+            children: [{
+              label: '三级 2-2-1'
+            }]
+          }]
+        }, {
+          label: '一级 3',
+          children: [{
+            label: '二级 3-1',
+            children: [{
+              label: '三级 3-1-1'
+            }]
+          }, {
+            label: '二级 3-2',
+            children: [{
+              label: '三级 3-2-1'
+            }]
+          }]
+        }],
+        defaultProps: {
+          children: 'children',
+          label: 'label'
+        }
+      }
+    },
+    methods: {
+      handleNodeClick (data) {
+        console.log(data)
+      },
+      getMenu () {
+        this.$http({
+          url: this.$http.adornUrl('/product/category/list/tree'),
+          method: 'get'
+        }).then(data => {
+          console.log('get menu date success: ', data)
+        })
+      }
+    },
+    created () {
+      this.getMenu()
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
